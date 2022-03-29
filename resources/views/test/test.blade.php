4456811fe0e3674e4491bf72f574d0fcdc1e1d76
angelinalos <newyorksquirrel33@gmail.com>
2022-03-29 19:07:52 +0300

    (ref): add name

--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -6,11 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>Фио</title>
 </head>
 <body>
-<div>
-<h1 class="text-green-500"> tee </h1>
+<div class="flex justify-center p-8 bg-purple-200 font-serif h-screen">
+<div class="pt-2 text-center shadow-md shadow-blue-400/50 w-96 h-14 rounded-md">
+    <h1 class="text-purple-900 text-3xl"> Лось Ангелина Юрьевна </h1>
+</div>
 </div>
 
 </body>
