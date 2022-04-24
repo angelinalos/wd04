907a33fccf20e8bd2806725fdbfcdee2f128190e
angelinalos <newyorksquirrel33@gmail.com>
2022-04-24 13:04:40 +0300

    (feat): CRUD products

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
 public function index(){
     $categories = Category::where('parent_category', 0)->get();
-    return view('site.index', compact('categories'));
+    $products = Product::where('category_id', 0)->get();
+
+    return view('site.index', compact('categories', 'products'));
 }
 }
